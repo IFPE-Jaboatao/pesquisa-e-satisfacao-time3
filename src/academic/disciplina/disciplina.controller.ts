b8ffc5baceaeb73174dbd8937d594941c966624c
Adilla <adillalopes2004@gmail.com>
2026-05-03 18:01:21 -0300

    feat(disciplina): adiciona campos e trata duplicação
    - adiciona campos codigo, createdAt, updatedAt e deletedAt
    - impede duplicação de nome, codigo e curso
    - implementa soft delete
    - trata erros 500 por reqs com :id não numérico
    - impede PATCH com body vazio

--- src/academic/disciplina/disciplina.controller.ts
+++ src/academic/disciplina/disciplina.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards, ParseIntPipe, BadRequestException } from '@nestjs/common';
 import { DisciplinaService } from './disciplina.service';
 import { CreateDisciplinaDto } from './dto/create-disciplina.dto';
 import { UpdateDisciplinaDto } from './dto/update-disciplina.dto';
@@ -28,21 +28,25 @@ export class DisciplinaController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':id')
   @Roles(Role.ADMIN, Role.GESTOR)
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
     return this.disciplinaService.findOne(+id);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
   @Roles(Role.ADMIN)
-  update(@Param('id') id: string, @Body() updateDisciplinaDto: UpdateDisciplinaDto) {
+  update(@Param('id', ParseIntPipe) id: string, @Body() updateDisciplinaDto: UpdateDisciplinaDto) {
+    if (Object.keys(updateDisciplinaDto).length === 0) {
+      throw new BadRequestException('Não foram fornecidos dados para atualização.');
+    }
+
     return this.disciplinaService.update(+id, updateDisciplinaDto);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   @Roles(Role.ADMIN)
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: string) {
     return this.disciplinaService.remove(+id);
   }
 }
--- src/academic/disciplina/entities/disciplina.entity.ts
+++ src/academic/disciplina/entities/disciplina.entity.ts
@@ -1,5 +1,5 @@
 import { Curso } from "src/academic/curso/entities/curso.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class Disciplina {
@@ -9,6 +9,18 @@ export class Disciplina {
 
     @Column()
     nome!: string
+
+    @Column({nullable: false})
+    codigo!: string
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt!: Date;
+
+    @DeleteDateColumn({ type: 'timestamp', nullable: true })
+    deletedAt?: Date;
 
     @ManyToOne(() => Curso, (curso) => curso.disciplinas, {
         onDelete: "CASCADE"
