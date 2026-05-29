cbe76ac34e9d52c32ac1008c4a868691e31e4030
adilla-somnia <adillalopes2004@gmail.com>
2026-05-29 20:29:36 -0300

    feat: configura landing page inicial
    - adiciona paleta de cores do projeto em styles/theme.css
    - configura header de landing page com logo

---
+++ frontend/app/_components/Header.tsx
@@ -0,0 +1,18 @@
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import Link from "next/link";
+
+export default function HeaderLandingPage() {
+    return (
+        <Navbar style={{ backgroundColor: 'var(--color-primary)'}}>
+            <NavbarBrand as={Link} href="/">
+            <img src="/pesquisa-logo.svg" className="mr-3 h-6 sm:h-12" alt="Pesquisas e Avaliações Logo" />
+        </NavbarBrand>
+        <NavbarToggle />
+        <NavbarCollapse>
+            <NavbarLink as={Link} href="/login">
+            Entrar
+            </NavbarLink>
+        </NavbarCollapse>
+        </Navbar>
+    )
+}
